518d8db54495b08ba3b3724fbdd2a89b86c4d7ba
emad hashem <eali32028@gmail.com>
2023-03-15 23:03:37 +0200

    adding get reservatino by id

--- src/routes/reservation.route.ts
+++ src/routes/reservation.route.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { cancelReservationHandler, createNewReservationHandler, getAllReservationHandler } from "../controllers/reservation.controller";
-import { success } from "../global";
 import { deserializeUser } from "../middlewares/deserializeUser.middleware";
 import { requiredUser } from "../middlewares/requiredUser.middleware";
 import { validate } from "../middlewares/validate.middleware";
@@ -11,6 +10,8 @@ const router = Router()
 router.use(deserializeUser, requiredUser)
 router.post('/add', validate(createNewReservationSchema), createNewReservationHandler)
 router.delete('/cancel', validate(deleteReservationSchema), cancelReservationHandler)
-router.get('/myReservations' , getAllReservationHandler)
-
+router.get('/all_user_reservation' , getAllReservationHandler)
+router.get('/get_reservation_by_id/:reservation_id' , (req , res) => {
+    
+})
 export default router
